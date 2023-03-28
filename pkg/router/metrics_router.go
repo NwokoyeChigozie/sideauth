7aa329efbfaf4cc45946ac51881d734e3ffe1011
Abubakar Hassan <abubakarhassan59@gmail.com>
2023-03-28 16:23:47 +0100

    Add metrics router package

---
+++ pkg/router/metrics_router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func SetupMetrics() *gin.Engine {
+	if appConfiguration.Mode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	return r
+}
