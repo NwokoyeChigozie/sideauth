7de2e098924d5db41be40b1d4d5772511637486d
Abubakar Hassan <abubakarhassan59@gmail.com>
2023-03-28 18:51:21 +0100

    Separated metrics route

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"log"
-
 	"github.com/vesicash/auth-ms/internal/config"
 	"github.com/vesicash/auth-ms/internal/models/migrations"
 	"github.com/vesicash/auth-ms/pkg/repository/storage/postgresql"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
 
 	"github.com/vesicash/auth-ms/utility"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/vesicash/auth-ms/pkg/router"
+)
+
+var (
+	g errgroup.Group
 )
 
 func main() {
@@ -27,7 +32,29 @@ func main() {
 	}
 
 	r := router.Setup(logger, validatorRef, db, &configuration.App)
+	rM := router.SetupMetrics(&configuration.App)
 
-	utility.LogAndPrint(logger, "Server is starting at 127.0.0.1:%s", configuration.Server.Port)
-	log.Fatal(r.Run(":" + configuration.Server.Port))
+	server := &http.Server{
+		Addr:    ":" + configuration.Server.Port,
+		Handler: r,
+	}
+
+	metricsServer := &http.Server{
+		Addr:    ":" + configuration.Server.MetricsPort,
+		Handler: rM,
+	}
+
+	g.Go(func() error {
+		utility.LogAndPrint(logger, "Server is starting at 127.0.0.1:%s", configuration.Server.Port)
+		return server.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		utility.LogAndPrint(logger, "Metrics Server is starting at 127.0.0.1:%s", "8015")
+		return metricsServer.ListenAndServe()
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
