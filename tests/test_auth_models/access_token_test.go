443b7fc46b5a8ab1e731ddbdc37bc1ed8ae2f4b9
Nwokoye Chigozie <gregoflash05@gmail.com>
2023-04-17 13:04:39 +0100

    added get access token by business id

--- tests/test_auth_models/access_token_test.go
+++ tests/test_auth_models/access_token_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -191,6 +192,126 @@ func TestGetAccessTokenByKey(t *testing.T) {
 			var b bytes.Buffer
 			json.NewEncoder(&b).Encode(test.RequestBody)
 			URI := url.URL{Path: "/v2/get_access_token_by_key/" + test.Key}
+
+			req, err := http.NewRequest(http.MethodGet, URI.String(), &b)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for i, v := range test.Headers {
+				req.Header.Set(i, v)
+			}
+
+			rr := httptest.NewRecorder()
+			r.ServeHTTP(rr, req)
+
+			tst.AssertStatusCode(t, rr.Code, test.ExpectedCode)
+
+			data := tst.ParseResponse(rr)
+
+			code := int(data["code"].(float64))
+			tst.AssertStatusCode(t, code, test.ExpectedCode)
+
+			if test.Message != "" {
+				message := data["message"]
+				if message != nil {
+					tst.AssertResponseMessage(t, message.(string), test.Message)
+				} else {
+					tst.AssertResponseMessage(t, "", test.Message)
+				}
+
+			}
+
+		})
+
+	}
+
+}
+
+func TestGetAccessTokenByBusinessID(t *testing.T) {
+	logger := tst.Setup()
+	app := config.GetConfig().App
+	gin.SetMode(gin.TestMode)
+	validatorRef := validator.New()
+	db := postgresql.Connection()
+	var (
+		muuid, _       = uuid.NewV4()
+		userSignUpData = models.CreateUserRequestModel{
+			EmailAddress: fmt.Sprintf("testuser%v@qa.team", muuid.String()),
+			PhoneNumber:  fmt.Sprintf("+234%v", utility.GetRandomNumbersInRange(8000000000, 9099999999)),
+			AccountType:  "individual",
+			Firstname:    "test",
+			Lastname:     "user",
+			Password:     "password",
+			Country:      "nigeria",
+			Username:     fmt.Sprintf("test_username%v", muuid.String()),
+		}
+		loginData = models.LoginUserRequestModel{
+			Username:     userSignUpData.Username,
+			EmailAddress: userSignUpData.EmailAddress,
+			PhoneNumber:  userSignUpData.PhoneNumber,
+			Password:     userSignUpData.Password,
+		}
+	)
+
+	auth := auth.Controller{Db: db, Validator: validatorRef, Logger: logger}
+	ra := gin.Default()
+	tst.SignupUser(t, ra, auth, userSignUpData)
+	_, accountID := tst.GetLoginTokenAndAccountID(t, ra, auth, loginData)
+	us := models.User{AccountID: uint(accountID)}
+	_, err := us.GetUserByAccountID(db.Auth)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	r := gin.Default()
+	aToken := models.AccessToken{AccountID: accountID}
+	err = aToken.CreateAccessToken(db.Auth)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		Name         string
+		RequestBody  interface{}
+		ExpectedCode int
+		Headers      map[string]string
+		Message      string
+		BusinessID   string
+	}{
+		{
+			Name:         "OK get access token by business id",
+			ExpectedCode: http.StatusOK,
+			Message:      "successful",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+				"v-app":        app.Key,
+			},
+			BusinessID: strconv.Itoa(aToken.AccountID),
+		},
+		{
+			Name:         "wrong key",
+			ExpectedCode: http.StatusBadRequest,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+				"v-app":        app.Key,
+			},
+			BusinessID: "wrong",
+		},
+	}
+
+	auth_model := auth_model.Controller{Db: db, Validator: validatorRef, Logger: logger}
+
+	authTypeUrl := r.Group(fmt.Sprintf("%v", "v2"), middleware.Authorize(db, middleware.AppType))
+	{
+		authTypeUrl.GET("/get_access_token_by_busines_id/:business_id", auth_model.GetAccessTokenByBusinessID)
+
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			var b bytes.Buffer
+			json.NewEncoder(&b).Encode(test.RequestBody)
+			URI := url.URL{Path: "/v2/get_access_token_by_busines_id/" + test.BusinessID}
 
 			req, err := http.NewRequest(http.MethodGet, URI.String(), &b)
 			if err != nil {
